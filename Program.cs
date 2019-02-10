0d8cddb70195f377bdad759f58ff8120f828f517
msi <martin.s.nielsen@simcorp.com>
2019-02-10 16:18:56 +0100

    Timer improvement, solution files added

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System.Threading.Tasks;
 
-namespace CompositeVideoOscilloscope
-{
+namespace CompositeVideoOscilloscope {
     class Program {
         static async Task Main(string[] args) {
-            var timer = new Timer(new PalTiming());
+            var timing = new PalTiming();
+            var timer = new Timer(minTime: 2 * timing.DotTime, maxTime: timing.FrameTime);
             double simulatedTime = 0;
             while (true) {
                 simulatedTime += await timer.GetElapsedTimeAsync();
-                // TODO composite signal generation
+                // TODO sample input
+                // TODO generate picture
+                // TODO generate composite signal
             }
         }
     }
--- Timer.cs
+++ Timer.cs
@@ -4,23 +4,22 @@ using System.Threading.Tasks;
 namespace CompositeVideoOscilloscope {
 
     public class Timer {
-        readonly Timing Timing;
         readonly Stopwatch StopWatch;
+        readonly double MinTime, MaxTime;
         double SimulatedTime;
 
-        public Timer(Timing timing) {
-            Timing = timing;
+        public Timer(double minTime, double maxTime) {
+            MinTime = minTime;
+            MaxTime = maxTime;
             StopWatch = new Stopwatch();
             StopWatch.Start();
             SimulatedTime = 0;
         }
 
         public async Task<double> GetElapsedTimeAsync() {
-            double minTime = 2 * Timing.DotTime;
-
-            while (StopWatch.Elapsed.TotalSeconds - SimulatedTime < minTime) {
-                if (minTime > 0.001) {
-                    await Task.Delay((int)(minTime * 1000));
+            while (StopWatch.Elapsed.TotalSeconds - SimulatedTime < MinTime) {
+                if (MinTime > 0.001) {
+                    await Task.Delay((int)(MinTime * 1000));
                 } else {
                     await Task.Yield();
                 }
@@ -30,12 +29,12 @@ namespace CompositeVideoOscilloscope {
             var elapsedTime = (tmpTime - SimulatedTime);
             SimulatedTime = tmpTime;
 
-            if (elapsedTime > 2.0 * Timing.FrameTime) {
-                var skipTime = (elapsedTime - Timing.FrameTime);
+            if (elapsedTime > 2.0 * MaxTime) {
+                var skipTime = (elapsedTime - MaxTime);
                 SimulatedTime += skipTime;
-                elapsedTime = Timing.FrameTime;
+                elapsedTime = MaxTime;
             }
-            
+
             return elapsedTime;
         }
     }
