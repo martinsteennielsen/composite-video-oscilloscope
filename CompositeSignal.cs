d733e502456cd3c0df9d71e5531f0c1de03e4f8d
Martin Steen Nielsen <martin.s.nielsen@simcorp.com>
2019-02-14 18:04:34 +0100

    Simple hSync added. Output is  now a square test pattern

--- CompositeSignal.cs
+++ CompositeSignal.cs
@@ -5,35 +5,50 @@ namespace CompositeVideoOscilloscope {
     public class CompositeSignal {
         readonly TimingConstants Timing;
         readonly Random Randomizer = new Random();
-        readonly ISignal HSync, HBlank;
+        readonly ISignal HSync, HBlank, VSync;
 
         public CompositeSignal(TimingConstants timing) {
             Timing = timing;
             HBlank = new SquareSignal(frequency: Timing.HFreq, onTime: Timing.SyncTimes.LineBlankingTime);
             HSync = new SquareSignal(frequency: Timing.HFreq, onTime: Timing.SyncTimes.LineSyncTime, onStartTime: Timing.SyncTimes.FrontPorchTime);
+            VSync = new SquareSignal(frequency: Timing.VFreq, onTime: Timing.FrameTime - 50 * Timing.LineTime);
         }
 
         public double Get(double time) => Randomizer.NextDouble();
 
         private double SimulatedTime = 0;
 
+        double? SyncValue(double simulatedTime) {
+
+            bool isHBlank() => HBlank.Get(simulatedTime) == 1;
+            bool isVBlank() => VSync.Get(SimulatedTime) == 1;
+
+            double? syncValue(double t) =>
+                isHBlank() ? (double?)(HSync.Get(t) == 1 ? 0 : 0.3) : null;
+
+            if (isVBlank()) {
+                return syncValue(SimulatedTime);
+            } else {
+                return syncValue(SimulatedTime) ?? 0.3;
+            }
+        }
+
+
+        double PixelValue(double simulatedTime) {
+            double w = Timing.LineTime / Timing.DotTime;
+            int x = (int)(SimulatedTime % Timing.LineTime / Timing.DotTime);
+            int y = (int)(SimulatedTime % Timing.FrameTime / Timing.LineTime);
+            double step(double v) => Math.Ceiling(10.0 * (double)v / w) / 10.0;
+            return step(y) * step(x) * 0.7 + 0.3;
+        }
+
         public List<double> Generate(double endTime, double skipTime) {
             SimulatedTime += skipTime;
             endTime += skipTime;
             var result = new List<double>();
-            int startX = (int)(SimulatedTime % Timing.LineTime / Timing.DotTime);
-            int startY = (int)(SimulatedTime % Timing.FrameTime / Timing.LineTime);
-            int x = startX;
-            int y = startY;
             while (SimulatedTime < endTime) {
-                double val = 0.3 + 0.7 * Get(x * Timing.DotTime + y * Timing.LineTime);
-                if (HBlank.Get(SimulatedTime) == 1) {
-                    val = HSync.Get(SimulatedTime) == 1 ? 0 : 0.3;
-                }
-                if (y > 300) { val = 0; }
-                result.Add(val);
-                x++; if (x > Timing.LineTime / Timing.DotTime) { x = 0; y++; }
-                if (y > Timing.FrameTime / Timing.LineTime) { y = 0; }
+                double signalValue = SyncValue(SimulatedTime) ?? PixelValue(SimulatedTime);
+                result.Add(signalValue);
                 SimulatedTime += Timing.DotTime;
             }
             return result;
