b9f4b7778d995abd1687b00cc6d2c8453f6bdd51
martinsteennielsen <martin.steen.nielsen@gmail.com>
2019-03-16 16:19:05 +0100

    Improve rendering of signal

--- Layer.cs
+++ Layer.cs
@@ -8,7 +8,7 @@ namespace CompositeVideoOscilloscope {
 
     public abstract class Layer: ILayer {
         readonly double ViewMinX, ViewMaxX, ViewMinY, ViewMaxY, ViewWidth, ViewHeight;
-        readonly double ScrWidth, ScrHeight, ScaleX, ScaleY, OffX, OffY;
+        readonly double ScrWidth, ScrHeight, ScaleX, ScaleY;
 
         public Layer(TimingConstants timing, double minX, double maxX, double minY, double maxY) {
             ViewMinX = minX; ViewMaxX = maxX; ViewMinY = minY; ViewMaxY = maxY;
@@ -17,34 +17,42 @@ namespace CompositeVideoOscilloscope {
             ScrHeight = 2d * timing.HFreq / timing.VFreq;
             ScaleX = ViewWidth / ScrWidth;
             ScaleY = ViewHeight / ScrHeight;
-            OffX = ViewMinX + ViewWidth / 2;
-            OffY = ViewMinY + ViewHeight / 2;
         }
 
         public double PixelValue(int x, int y, double currentValue) {
-            return currentValue * Value( (x * ScaleX) + ViewMinX, (y * ScaleY) + ViewMinY);
+            return currentValue * Value( (x * ScaleX) + ViewMinX, (y * ScaleY) + ViewMinY, ScaleX, ScaleY);
         }
-        protected abstract double Value(double x, double y);
+        protected abstract double Value(double x, double y, double dx, double dy);
     }
 
     public class AxisLayer : Layer {
         public AxisLayer(TimingConstants timing) : base(timing, 0, 200, 0, 200) { }
 
-        protected override double Value(double x, double y) {
-            if (Math.Abs(x % 20) < 0.4) { return 0; }
-            if (Math.Abs(y % 20) < 0.2) { return 0; }
+        protected override double Value(double x, double y, double dx, double dy) {
+            if (Math.Abs(x % 20) < dx) { return 0; }
+            if (Math.Abs(y % 20) < dy) { return 0; }
             return 1;
         }
     }
 
     public class BackgroundLayer : Layer {
         public BackgroundLayer(TimingConstants timing) : base(timing, 0, 1, 0, 1) { }
-        protected override double Value(double x, double y) => .2;
+        protected override double Value(double x, double y, double dx, double dy) => .2;
     }
 
     public class SignalLayer : Layer {
         public SignalLayer(TimingConstants timing) : base(timing, 0, 20, -2, 2) { }
-        protected override double Value(double x, double y) => Math.Abs(Math.Sin(x) - y) < 0.02 ? 10 : 1;
+        protected override double Value(double t, double v, double dt, double dv) {
+            double s1 = Signal(t);
+            double s2 = Signal(t + dt);
+            int d1 = s1 - v > 0 ? 1 : -1;
+            int d2 = s2 - v > 0 ? 1 : -1;
+            int d3 = s1 - v + dv > 0 ? 1 : -1;
+            int d4 = s2 - v + dv > 0 ? 1 : -1;
+            return Math.Abs(d1 + d2 + d3 + d4) == 4 ? 1 : 10;
+        }
+
+        double Signal(double t) => Math.Sin(t) - 0.5 * Math.Sin(t*3);
     }
 
 }
