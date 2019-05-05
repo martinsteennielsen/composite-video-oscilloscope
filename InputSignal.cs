19cf5eb8fa57f292d6cdb4c19229d4388becdcf2
martinsteennielsen <martin.steen.nielsen@gmail.com>
2019-05-05 18:47:07 +0200

    Improve performance

--- InputSignal.cs
+++ InputSignal.cs
@@ -14,18 +14,30 @@ namespace CompositeVideoOscilloscope {
                 Buffer[idx] = Generate(time); 
             }
         }
-        public bool TryGet(double time, out double value) {
-            value=0;
-            if (time < StartTime) { 
+
+        double[] Values = new double[] {0,0,0,0,0};
+
+        public bool TryGet(double time, double dt, out double[] values) {
+            values=Values;
+            if (time - 2*dt < StartTime) { 
                 return false; 
             }
-            if (time >= EndTime) { 
+            if (time + 2*dt >= EndTime) { 
                 return false;
             }
-            int bufpos = (int)((time-StartTime)/SampleTime);
-            value = Buffer[bufpos];
+            int bufpos = (int)((time-StartTime-2*dt)/SampleTime);
+            values[0] = Buffer[bufpos];
+            bufpos = (int)((time-StartTime-dt)/SampleTime);
+            values[1] = Buffer[bufpos];
+            bufpos = (int)((time-StartTime)/SampleTime);
+            values[2] = Buffer[bufpos];
+            bufpos = (int)((time-StartTime + dt)/SampleTime);
+            values[3] = Buffer[bufpos];
+            bufpos = (int)((time-StartTime + 2*dt)/SampleTime);
+            values[4] = Buffer[bufpos];
             return true;
         }
+
         private double Generate(double t)=> Math.Sin(t) - 0.5 * Math.Sin(t * 3);
     }
 }
--- LayerSignal.cs
+++ LayerSignal.cs
@@ -3,32 +3,36 @@ namespace CompositeVideoOscilloscope {
     public class LayerSignal : IScreenContent {
         private readonly View View;
         private readonly InputSignal Signal;
+        private readonly double dT, dV, d2T, d2v;
 
         public LayerSignal(ScreenResolution resolution, InputSignal signal) {
             Signal = signal;
-            View = new View(-20, 40, -2, 2, resolution: new ScreenResolution(2 * resolution.Width, 2 * resolution.Height));
+            View = new View(20, 30, -2, 2, resolution: new ScreenResolution(2 * resolution.Width, 2 * resolution.Height));
+            dT=View.Scaler.dX;
+            d2T=2*dT;
+            dV = View.Scaler.dY;
+            d2v=2*dV;
         }
 
         public int PixelValue(int x, int y) => Value(x << 1, y << 1);
 
         private int Value(int x, int y) {
-            (double time, double voltage) = View.Transform(x, y);
+            (double t, double v) = View.Transform(x, y);
+            double[] ss;
+            if (!Signal.TryGet(t,dT,out ss)) { return 255; }
+            double vu = v-dV, vuu = v-d2v, vd = v+dV, vdd = v+d2v;
+            bool dr=ss[3]-v>0, dd=ss[2]-vd>0, du=ss[2]-vu>0, dl=ss[1]-v>0;
 
-            int s = IsPixelOn(time - View.Scaler.dX, voltage - View.Scaler.dY) ? 4: 1;
-            s += IsPixelOn(time + View.Scaler.dX, voltage - View.Scaler.dY) ? 4: 1;
-            s += IsPixelOn(time - View.Scaler.dX, voltage + View.Scaler.dY) ? 4: 1;
-            s += IsPixelOn(time + View.Scaler.dX, voltage + View.Scaler.dY) ? 4: 1;
-            s += (IsPixelOn(time, voltage) ? 4:1) << 2;
-            return s << 5;
+            int r=0;
+            r += ( IsOff(dd,       du,       dl,       dr)            ? 1:4 ) << 2;
+            r +=   IsOff(dl,       ss[1]-vuu>0, ss[0]-vu>0, du)       ? 1:4;
+            r +=   IsOff(dr,       ss[3]-vuu>0, du,       ss[4]-vu>0) ? 1:4;
+            r +=   IsOff(ss[1]-vdd>0, dl,       ss[0]-vd>0, dd)       ? 1:4;
+            r +=   IsOff(ss[3]-vdd>0, dr,       dd,       ss[4]-vd>0) ? 1:4;
+            return r << 5;
         }
 
-        private bool IsPixelOn(double time, double voltage) {
-            if (!Signal.TryGet(time, out double value)) { return false; }
-            sbyte s = value - voltage - View.Scaler.dY > 0 ? (sbyte)1 : (sbyte)-1;
-            s += value - voltage + View.Scaler.dY > 0 ? (sbyte)1 : (sbyte)-1;
-            s += (Signal.TryGet(time - View.Scaler.dX, out double v1) ? v1 : value) - voltage > 0 ? (sbyte)1 : (sbyte)-1;
-            s += (Signal.TryGet(time + View.Scaler.dX, out double v2) ? v2 : value) - voltage > 0 ? (sbyte)1 : (sbyte)-1;
-            return s != 4 && s != -4;
-        }
+        private bool IsOff(bool dd, bool du, bool dl, bool dr) => 
+            (dd && du && dl && dr) || !(dd || du || dl || dr);
     }
 }
