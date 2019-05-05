eb31d9dec119424f5dd137299c0ad3ba78726aa9
martinsteennielsen <martin.steen.nielsen@gmail.com>
2019-05-05 13:21:09 +0200

    Improve performance

--- LayerSignal.cs
+++ LayerSignal.cs
@@ -1,46 +1,34 @@
-﻿using System;
-
+﻿
 namespace CompositeVideoOscilloscope {
     public class LayerSignal : IScreenContent {
         private readonly View View;
-        private readonly InputSignal Signal; 
+        private readonly InputSignal Signal;
 
         public LayerSignal(ScreenResolution resolution, InputSignal signal) {
             Signal = signal;
-            View = new View(20, 40, -1, 1, resolution: new ScreenResolution(2*resolution.Width, 2*resolution.Height));
+            View = new View(-20, 40, -2, 2, resolution: new ScreenResolution(2 * resolution.Width, 2 * resolution.Height));
         }
 
-        public int PixelValue(int x, int y) => Value(2*x, 2*y);
+        public int PixelValue(int x, int y) => Value(x << 1, y << 1);
 
         private int Value(int x, int y) {
             (double time, double voltage) = View.Transform(x, y);
 
-             if (!Signal.TryGet(time, out double dummy)) {
-                 return 255;
-             } 
-             
-            var p1 = SubValue(time - View.Scaler.dX, voltage - View.Scaler.dY);
-            var p2 = SubValue(time + View.Scaler.dX, voltage - View.Scaler.dY);
-            var p3 = SubValue(time - View.Scaler.dX, voltage + View.Scaler.dY);
-            var p4 = SubValue(time + View.Scaler.dX, voltage + View.Scaler.dY);
-            var p5 = SubValue(time, voltage) << 2;
-
-            return (int)(255*( p1 + p2 + p3 + p4 + p5 ) / 8);
+            int s = IsPixelOn(time - View.Scaler.dX, voltage - View.Scaler.dY) ? 4: 1;
+            s += IsPixelOn(time + View.Scaler.dX, voltage - View.Scaler.dY) ? 4: 1;
+            s += IsPixelOn(time - View.Scaler.dX, voltage + View.Scaler.dY) ? 4: 1;
+            s += IsPixelOn(time + View.Scaler.dX, voltage + View.Scaler.dY) ? 4: 1;
+            s += (IsPixelOn(time, voltage) ? 4:1) << 2;
+            return s << 5;
         }
 
-        private int SubValue(double time, double voltage) {
-            if (!Signal.TryGet(time, out double value)) { return 1;}
-
-            double s1 = value - voltage - View.Scaler.dY;
-            double s2 = value - voltage + View.Scaler.dY;
-            double s3 = ( Signal.TryGet(time - View.Scaler.dX, out double v1) ? v1 : value) - voltage;
-            double s4 = ( Signal.TryGet(time + View.Scaler.dX, out double v2) ? v2 : value) - voltage;
-            int d1 = s1 > 0 ? 1 : -1;
-            int d2 = s2 > 0 ? 1 : -1;
-            int d3 = s3 > 0 ? 1 : -1;
-            int d4 = s4 > 0 ? 1 : -1;
-            return Math.Abs(d1 + d2 + d3 + d4) == 4 ? 1 : 4;
+        private bool IsPixelOn(double time, double voltage) {
+            if (!Signal.TryGet(time, out double value)) { return false; }
+            sbyte s = value - voltage - View.Scaler.dY > 0 ? (sbyte)1 : (sbyte)-1;
+            s += value - voltage + View.Scaler.dY > 0 ? (sbyte)1 : (sbyte)-1;
+            s += (Signal.TryGet(time - View.Scaler.dX, out double v1) ? v1 : value) - voltage > 0 ? (sbyte)1 : (sbyte)-1;
+            s += (Signal.TryGet(time + View.Scaler.dX, out double v2) ? v2 : value) - voltage > 0 ? (sbyte)1 : (sbyte)-1;
+            return s != 4 && s != -4;
         }
-
     }
 }
